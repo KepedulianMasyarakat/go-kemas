3c1b0c159a9288cfe8ddaed419a7b5d80d33bf7b
viantotech <viantonugroho@student.gunadarma.ac.id>
2021-09-20 14:00:25 +0700

    Update config.go
    config update

--- config/config.go
+++ config/config.go
@@ -6,7 +6,7 @@ import (
 
 func SetupDB() *gorm.DB {
   // refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
-  dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
+  dsn := "user:pass@tcp(localhost:433)/kemas_go?charset=utf8mb4&parseTime=True&loc=Local"
   db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 	panic(err.Error())
